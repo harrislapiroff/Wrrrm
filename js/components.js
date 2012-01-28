0c040bcbe406136f889e5bcf1197c0e8f57073c1
Harris Lapiroff <harris@chromamine.com>
2012-01-28 12:57:04 -0500

    Separated planet and planetwalker into different components.

--- js/components.js
+++ js/components.js
@@ -14,23 +14,45 @@
   });
   Crafty.c("Snake", {
     init: function() {
-      return this.requires("2D, DOM, Tween");
+      return this.requires("2D, DOM, Tween, Planet");
     },
     snake: function(radius, rotation_frames, stroke) {
       this.radius = radius;
       this.rotation_frames = rotation_frames;
       this.stroke = stroke != null ? stroke : 40;
+      this.planet(this.radius, this.stroke);
+      return this.origin(this.radius, this.radius);
+    }
+  });
+  Crafty.c("SnakePart", {
+    init: function() {
+      return this.requires("2D, DOM, Tween, PlanetWalker");
+    },
+    snakepart: function(snake, circumference_location, altitude) {
+      this.snake = snake;
+      this.circumference_location = circumference_location;
+      this.altitude = altitude != null ? altitude : 0;
+      this.planetwalker(this.snake, 0, this.altitude + this.snake.radius);
+      return this.setCircumferenceLocation(this.circumference_location);
+    }
+  });
+  Crafty.c("Planet", {
+    init: function() {
+      return this.requires("2D, DOM");
+    },
+    planet: function(radius, stroke) {
+      this.radius = radius;
+      this.stroke = stroke;
       this.attr({
         x: Crafty.viewport.width / 2 - this.radius,
         y: Crafty.viewport.height - 200,
         w: (this.radius - this.stroke) * 2,
         h: (this.radius - this.stroke) * 2
       });
-      this.css({
+      return this.css({
         'border-radius': this.radius,
         'border': "" + this.stroke + "px solid #000"
       });
-      return this.origin(this.radius, this.radius);
     },
     startspin: function() {
       this.attr({
@@ -44,45 +66,50 @@
     stopspin: function() {
       return this.trigger("StopSpin");
     },
+    getCartesianCoords: function() {
+      return this.pos();
+    },
     circumference: function() {
       return this.radius * 6.28;
     }
   });
-  Crafty.c("SnakePart", {
+  Crafty.c("PlanetWalker", {
     init: function() {
-      return this.requires("2D, DOM, Tween");
+      return this.requires("2D, DOM");
     },
-    snakepart: function(snake, circumference_location, altitude) {
-      var pos, snake_d, snake_pos, snake_r;
-      this.snake = snake;
-      this.circumference_location = circumference_location;
-      this.altitude = altitude != null ? altitude : 0;
-      this.initial_rotation = this.circumference_location * 360 / this.snake.circumference();
-      snake_pos = this.snake.pos();
-      snake_r = this.snake.radius;
-      snake_d = snake_r * 2;
-      pos = this.pos();
-      this.attr({
-        rotation: this.initial_rotation,
-        y: snake_pos._y - this.altitude,
-        x: (Crafty.viewport.width - pos._w) / 2
-      });
-      this.origin(pos._w / 2, snake_d / 2 + this.altitude);
-      this.snake.bind("StartSpin", __bind(function() {
+    planetwalker: function(planet, theta, r) {
+      this.planet = planet;
+      this.theta = theta;
+      this.r = r;
+      this.position(this.theta, this.r);
+      this.planet.bind("StartSpin", __bind(function() {
         return this._startspin();
       }, this));
-      return this.snake.bind("StopSpin", __bind(function() {
+      return this.planet.bind("StopSpin", __bind(function() {
         return this._stopspin();
       }, this));
     },
     _startspin: function(e) {
-      this.attr({
-        rotation: this.initial_rotation
-      });
       return this.tween({
-        rotation: this.initial_rotation - 360
+        rotation: this.theta - 360
       }, this.snake.rotation_frames);
     },
-    _stopspin: function() {}
+    _stopspin: function() {},
+    position: function(theta, r) {
+      this.theta = theta;
+      this.r = r;
+      this.altitude = this.r - this.planet.radius;
+      this.origin(this.pos()._w / 2, this.planet.radius + this.altitude);
+      return this.attr({
+        rotation: this.theta,
+        y: this.planet.pos()._y - this.altitude,
+        x: (Crafty.viewport.width - this.pos()._w) / 2
+      });
+    },
+    setCircumferenceLocation: function(circumference_location) {
+      var theta;
+      theta = circumference_location * 360 / this.planet.circumference();
+      return this.position(theta, this.r);
+    }
   });
 }).call(this);
--- src/components.coffee
+++ src/components.coffee
@@ -10,62 +10,81 @@ Crafty.c "Protagonist",
 
 Crafty.c "Snake"
 	init: () ->
-		@requires "2D, DOM, Tween"
+		@requires "2D, DOM, Tween, Planet"
 		
 	snake: (@radius, @rotation_frames, @stroke=40) ->
+		@planet @radius, @stroke
+		# set the radius and number of frames for a complete rotation for the snake.
+		@origin @radius, @radius
+
+
+Crafty.c "SnakePart",
+	init: () ->
+		@requires "2D, DOM, Tween, PlanetWalker"
+		
+	snakepart: (@snake, @circumference_location, @altitude=0) ->
+		# set which snake the part is attached to and where on the circumference of the snake the part is located
+		# please call this *after* setting a width and height on the entity and its snake parent
+		
+		# initialize the planetwalkerness
+		@planetwalker @snake, 0, @altitude + @snake.radius
+		@setCircumferenceLocation @circumference_location
+
+Crafty.c "Planet",
+	init: () ->
+		@requires "2D, DOM"
+	
+	planet: (@radius, @stroke) ->
 		@attr {x: Crafty.viewport.width/2 - @radius, y: Crafty.viewport.height - 200, w: (@radius-@stroke)*2, h: (@radius-@stroke)*2}
 		@css
 			'border-radius': @radius
 			'border': "#{@stroke}px solid #000"
-			
-		# set the radius and number of frames for a complete rotation for the snake.
-		@origin @radius, @radius
 	
 	startspin: () ->
 		# start the snake spinning
 		@attr {rotation: 0}
 		@tween {rotation: -360}, @rotation_frames
 		@trigger "StartSpin"
-	
+
 	stopspin: () ->
 		# unimplemented, because there's currently no method to stop the tween
 		@trigger "StopSpin"
 	
+	getCartesianCoords: () ->
+		return @pos()
+	
 	circumference: () ->
 		# returns the circumference of the snake
-		@radius*6.28
+		@radius * 6.28
 
-
-Crafty.c "SnakePart",
+Crafty.c "PlanetWalker",
 	init: () ->
-		@requires "2D, DOM, Tween"
-		
-	snakepart: (@snake, @circumference_location, @altitude=0) ->
-		# set which snake the part is attached to and where on the circumference of the snake the part is located
-		# please call this *after* setting a width and height on the entity and its snake parent
-		
-		# calculate and save the inital rotation
-		@initial_rotation = @circumference_location*360/@snake.circumference()
-		
-		# cache, briefly, the position variables
-		snake_pos = @snake.pos()
-		snake_r = @snake.radius
-		snake_d = snake_r*2
-		pos = @pos()
-		
-		# rotate the entity to be at the correct location on the snake
-		@attr {rotation: @initial_rotation, y: snake_pos._y-@altitude, x: (Crafty.viewport.width - pos._w)/2}
-		
-		# set the origin to the center of the snake
-		@origin pos._w/2, snake_d/2 + @altitude
+		@requires "2D, DOM"
+	
+	planetwalker: (@planet, @theta, @r) ->
+		# set default position
+		@position @theta, @r
 		
 		# bind the spinning to the snake spinning
-		@snake.bind "StartSpin", () => @_startspin()
-		@snake.bind "StopSpin", () => @_stopspin()
+		@planet.bind "StartSpin", () => @_startspin()
+		@planet.bind "StopSpin", () => @_stopspin()
 	
 	_startspin: (e) ->
-		@attr {rotation: @initial_rotation}
-		@tween {rotation: @initial_rotation-360}, @snake.rotation_frames
+		@tween {rotation: @theta - 360}, @snake.rotation_frames
 	
 	_stopspin: () ->
 		# unimplemented, because there's currently no method to stop the tween
+	
+	position: (@theta, @r) ->
+		# provide theta in degrees
+		@altitude = @r - @planet.radius
+		
+		# set the origin to the center of the planet
+		@origin @pos()._w/2, @planet.radius + @altitude
+		
+		# rotate the entity to be at the correct location on the snake
+		@attr {rotation: @theta, y: @planet.pos()._y - @altitude, x: (Crafty.viewport.width - @pos()._w)/2}
+	
+	setCircumferenceLocation: (circumference_location) ->
+		theta = circumference_location * 360 / @planet.circumference()
+		@position theta, @r
